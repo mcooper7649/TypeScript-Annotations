7f770747a72e8631c20918d3049b5669bf8d23da
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2021-12-09 23:12:12 -0500

    complete type annotation on functions

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -11,3 +11,30 @@ let nothing: undefined = undefined;
 //built in objects
 
 let now: Date = new Date();
+
+// Array
+
+let colors: string[] = ['red', 'green', 'blue'];
+
+let myNumber: number[] = [1, 2, 3];
+
+let truths: boolean[] = [true, true, false];
+
+// Classes
+
+class Car {}
+
+let car: Car = new Car();
+
+// Object literal
+
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
+
+// Function
+
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
