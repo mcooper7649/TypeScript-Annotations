51181bbbbb0bb8bb4a8eb93f424518d3fe78de7b
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2021-12-12 00:48:15 -0500

    complete tuples

--- annotations/tuples.ts
+++ annotations/tuples.ts
@@ -11,3 +11,10 @@ type Drink = [string, boolean, number];
 const pepsi: Drink = ['brown', true, 40];
 const sprite: Drink = ['clear', true, 40];
 const tea: Drink = ['brown', false, 0];
+
+const carSpecs: [number, number] = [400, 3354];
+
+const carStats = {
+  horsePower: 400,
+  weight: 3354,
+};
